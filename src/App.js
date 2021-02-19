5886fe19378406a2a8263d044c9edf999b5913e5
biagioo <mendolia2328@gmail.com>
2021-02-19 08:25:29 -0500

    stopped invoking props in Navbar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ class App extends Component {
   
 
   render(){
- 
-  
     return (
       <div className="App">
-       <Navbar title='Github Finder' icon='fab fa-github'/>
+       <Navbar />
       </div>
     );
   }
