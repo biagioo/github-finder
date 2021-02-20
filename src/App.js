d2e2005f47ca7af708af957e8d8882104fb030e1
biagioo <mendolia2328@gmail.com>
2021-02-20 09:31:03 -0500

    imported UserItem into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
        <Navbar />
+       <UserItem />
       </div>
     );
   }
