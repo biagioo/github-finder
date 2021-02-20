291aa55ba69402d3382dc2b4b720c396a7efb786
biagioo <mendolia2328@gmail.com>
2021-02-20 09:51:16 -0500

    rendered state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
 import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
-  
-
-  render(){
+  render() {
     return (
-      <div className="App">
-       <Navbar />
-       <UserItem />
+      <div className='App'>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
- 
 }
 
 export default App;
