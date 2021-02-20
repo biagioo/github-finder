1b0eeae69f92eb82fff99df211c69ef49ff5ae21
biagioo <mendolia2328@gmail.com>
2021-02-20 11:15:35 -0500

    removed state and destructered props

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
-	static defaultProps = {
-		title:'Github Finder',
-		icon:'fab fa-github'
-	}
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github',
+  };
 
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		icon: PropTypes.string.isRequired
-	}
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
 
   render() {
     return (
