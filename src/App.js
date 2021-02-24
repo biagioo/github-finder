7742948caf82dfbae42f63e336fc37790509b79f
biagioo <mendolia2328@gmail.com>
2021-02-23 19:32:49 -0500

    correct state in searchUsers, added propTypes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import './App.css';
 
@@ -11,6 +12,9 @@ class App extends Component {
     loading: false,
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
   async componentDidMount() {
     this.setState({ loading: true });
 
@@ -22,6 +26,8 @@ class App extends Component {
   }
   // search github users
   searchUsers = async text => {
+    this.setState({ loading: true });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
