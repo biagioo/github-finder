6d00ee0e32709b5bbe11dc029b35eda116975b02
biagioo <mendolia2328@gmail.com>
2021-03-10 10:41:29 -0500

    removed searchUsers from App.js

--- src/App.js
+++ src/App.js
@@ -17,18 +17,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // SEARCH GITHUB USERS
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // GET A SINGLE USER
   const getUser = async username => {
     setLoading(true);
@@ -80,7 +68,6 @@ const App = () => {
                 render={() => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
