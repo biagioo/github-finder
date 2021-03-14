49ef924da11b592cf4df80913c3a9013e9836fcd
biagioo <mendolia2328@gmail.com>
2021-03-14 13:26:19 -0400

    pulled repos from context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
 
-  const { user, loading, getUser } = githubContext;
+  const { getUser, user, getUserRepos, repos, loading } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -103,14 +103,6 @@ const User = ({ getUserRepos, repos, match }) => {
       </div>
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
 };
 
 export default User;
