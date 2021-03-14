738c0c31aed3869a1c04c788ff9d902e803e59ec
biagioo <mendolia2328@gmail.com>
2021-03-14 13:25:45 -0400

    refactored to use Context getUserRepos

--- src/App.js
+++ src/App.js
@@ -15,18 +15,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // GET USER REPOS
-  const getUserRepos = async username => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?&per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-
   // SHOW SEARCH ALERT
   const showAlert = (msg, type) => {
     setAlert({ msg, type });
@@ -56,9 +44,7 @@ const App = () => {
               <Route
                 exact
                 path='/user/:login'
-                render={props => (
-                  <User {...props} getUserRepos={getUserRepos} repos={repos} />
-                )}
+                render={props => <User {...props} />}
               />
             </Switch>
           </div>
